d8e1a43073566c7ca0034179df66a411b7149715
Ian M <ianmacalinao@gmail.com>
2012-04-03 10:15:01 -0500

    Memory functions.

--- source/cpu/Memory.js
+++ source/cpu/Memory.js
@@ -15,6 +15,84 @@
  * along with SNESJS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+SNESJS.CPU.prototype.op_readpc = function() {
+  return this.this.op_read((this.regs.pc.b << 16) + this.regs.pc.w++);
+}
+
+SNESJS.CPU.prototype.op_readstack = function() {
+  this.regs.e ? this.regs.s.l++ : this.regs.s.w++;
+  return this.op_read(this.regs.s.w);
+}
+
+SNESJS.CPU.prototype.op_readstackn = function() {
+  return this.op_read(++this.regs.s.w);
+}
+
+SNESJS.CPU.prototype.op_readaddr = function(addr) {
+  return this.op_read(addr & 0xffff);
+}
+
+SNESJS.CPU.prototype.op_readlong = function(addr) {
+  return this.op_read(addr & 0xffffff);
+}
+
+SNESJS.CPU.prototype.op_readdbr = function(addr) {
+  return this.op_read(((this.regs.db << 16) + addr) & 0xffffff);
+}
+
+SNESJS.CPU.prototype.op_readpbr = function(addr) {
+  return this.op_read((this.regs.pc.b << 16) + (addr & 0xffff));
+}
+
+SNESJS.CPU.prototype.op_readdp = function(addr) {
+  if (this.regs.e && this.regs.d.l == 0x00) {
+    return this.op_read((this.regs.d & 0xff00) + ((this.regs.d + (addr & 0xffff)) & 0xff));
+  } else {
+    return this.op_read((this.regs.d + (addr & 0xffff)) & 0xffff);
+  }
+}
+
+SNESJS.CPU.prototype.op_readsp = function(addr) {
+  return this.op_read((this.regs.s + (addr & 0xffff)) & 0xffff);
+}
+
+SNESJS.CPU.prototype.op_writestack = function(data) {
+  this.op_write(this.regs.s.w, data);
+  this.regs.e ? this.regs.s.l-- : this.regs.s.w--;
+}
+
+SNESJS.CPU.prototype.op_writestackn = function(data) {
+  this.op_write(this.regs.s.w--, data);
+}
+
+SNESJS.CPU.prototype.op_writeaddr = function(addr, data) {
+  this.op_write(addr & 0xffff, data);
+}
+
+SNESJS.CPU.prototype.op_writelong = function(addr, data) {
+  this.op_write(addr & 0xffffff, data);
+}
+
+SNESJS.CPU.prototype.op_writedbr = function(addr, data) {
+  this.op_write(((this.regs.db << 16) + addr) & 0xffffff, data);
+}
+
+SNESJS.CPU.prototype.op_writepbr = function(addr, data) {
+  this.op_write((this.regs.pc.b << 16) + (addr & 0xffff), data);
+}
+
+SNESJS.CPU.prototype.op_writedp = function(addr, data) {
+  if (this.regs.e && this.regs.d.l == 0x00) {
+    this.op_write((this.regs.d & 0xff00) + ((this.regs.d + (addr & 0xffff)) & 0xff), data);
+  } else {
+    this.op_write((this.regs.d + (addr & 0xffff)) & 0xffff, data);
+  }
+}
+
+SNESJS.CPU.prototype.op_writesp = function(addr, data) {
+  this.op_write((this.regs.s + (addr & 0xffff)) & 0xffff, data);
+}
+
 SNESJS.CPU.prototype.pio = function() {
   return this.status.pio;
 }
@@ -40,14 +118,14 @@ SNESJS.CPU.prototype.op_io = function() {
 }
 
 SNESJS.CPU.prototype.op_read = function(addr) {
-  this.regs.mdr = this.snes.bus.read(addr);
+  this.this.regs.mdr = this.snes.bus.read(addr);
   this.add_clocks(speed(addr));
-  return regs.mdr;
+  return this.regs.mdr;
 }
 
 SNESJS.CPU.prototype.op_write = function(addr, data) {
   this.add_clocks(speed(addr));
-  this.snes.bus.write(addr, regs.mdr = data);
+  this.snes.bus.write(addr, this.regs.mdr = data);
 }
 
 SNESJS.CPU.prototype.speed = function(addr) {
